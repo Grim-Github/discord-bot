67f055120b5f579fec3f68f6b8e07cb3d469f84c
GrimHub <vladutsava7@yahoo.com>
2020-11-06 19:15:40 +0200

    v1.2

--- Commands.cs
+++ Commands.cs
@@ -1,8 +1,11 @@
 ﻿using Discord;
+using Discord.Audio;
 using Discord.Commands;
 using Discord.WebSocket;
+using Discord.Rest;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace discord_bot
@@ -12,6 +15,9 @@ namespace discord_bot
         public static List<SocketGuildUser> RandomBanUser = new List<SocketGuildUser>();
         public static List<SocketGuildUser> RandomKickUser = new List<SocketGuildUser>();
         private Random rng = new Random();
+        private EmoteClass ec = new EmoteClass();
+
+        #region MISC
 
         [Command("spune")]
         public async Task Say([Remainder] string text)
@@ -36,9 +42,16 @@ namespace discord_bot
         [Command("ajutor")]
         public async Task Help()
         {
-            await Context.Channel.SendMessageAsync(">>> **Comenzi** : \n Bully (nume) (numar) (mesaj) \n Random Kick \n Random Ban");
+            await Context.Message.AddReactionAsync(ec.SendEmote(0));
+            await Context.Channel.SendMessageAsync(">>> **Comenzi** : \n Bully (nume) (numar) (mesaj) " +
+                "\n Spune (mesaj) " +
+                "\n Random Kick " +
+                "\n Random Ban " +
+                "\n Cafea");
         }
+        #endregion
 
+        #region Random BAN/KICK
         [Command("random ban")]
         public async Task RandomBanEnter()
         {
@@ -57,17 +70,18 @@ namespace discord_bot
             if (Context.User.Id == Program.ownerID)
             {
                 SocketGuildUser bannedUser = RandomBanUser[rng.Next(0, RandomBanUser.Count)];
+                RandomBanUser.Clear();
                 await bannedUser.SendMessageAsync("f");
                 await Context.Channel.SendMessageAsync(">>> Dori me Interimo adapare Dori me Ameno ameno " + bannedUser);
                 await bannedUser.BanAsync();
-                RandomBanUser.Clear();
+
             }
         }
 
         [Command("random kick")]
         public async Task RandomKickEnter()
         {
-            if (RandomBanUser.Contains(Context.User as SocketGuildUser))
+            if (RandomKickUser.Contains(Context.User as SocketGuildUser))
             {
                 await Context.Channel.SendMessageAsync(">>> Deja esti inscris");
                 return;
@@ -82,12 +96,49 @@ namespace discord_bot
             if (Context.User.Id == Program.ownerID)
             {
                 SocketGuildUser kickedUser = RandomKickUser[rng.Next(0, RandomKickUser.Count)];
+                RandomKickUser.Clear();
                 await kickedUser.SendMessageAsync("ez");
                 await Context.Channel.SendMessageAsync(">>> f " + kickedUser);
                 await kickedUser.KickAsync();
-                RandomKickUser.Clear();
             }
 
         }
+        #endregion
+
+        #region Voice
+        [Command("cafea", RunMode = RunMode.Async)]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+            await Context.Message.DeleteAsync();
+
+            var audioClient = await channel.ConnectAsync();
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg using the previous example
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+        #endregion
     }
 }
---
+++ EmoteClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+
+namespace discord_bot
+{
+    class EmoteClass
+    {
+        string[] emotelist = {"<:gabor:773165564049752064>"  };
+
+        public IEmote SendEmote(int value)
+        {
+            if (value <= emotelist.Length)
+            {
+                return Emote.Parse(emotelist[value]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
