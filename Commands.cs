b3a668f2e142a26fc13b051cc8d60923f6804cba
GrimHub <vladutsava7@yahoo.com>
2020-11-06 09:21:04 +0200

    v1

---
+++ Commands.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace discord_bot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("spune")]
+        public async Task Say([Remainder] string text)
+        {
+            await Context.Message.DeleteAsync();
+            await Context.Channel.SendMessageAsync(text);
+        }
+
+
+        [Command("bully" , RunMode = RunMode.Async)]
+        public async Task Bully(SocketGuildUser user, int amount , [Remainder]string text)
+        {
+            await Context.Message.DeleteAsync();
+            for (int i = 0; i < amount; i++)
+            {
+                await Context.Channel.SendMessageAsync(user.Mention + " " + text);
+                await user.SendMessageAsync(user.Mention + " " + text);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace discord_bot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static DiscordSocketClient _client = new DiscordSocketClient();
+        private CommandHandler commandHandler = new CommandHandler(_client , new Discord.Commands.CommandService());
+        private UserStatus botStatus = UserStatus.DoNotDisturb;
+        private string botPlayingStatus = "Cei ce vom izbandi, din incercarea aceasta....vom iesi mai puternici.....";
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, "Nzc0MTYwODY1MTY3NDA5MTUy.X6TvfQ.haMFSzuvPtIQBcr7oe56Eod3vpw");
+
+            await _client.StartAsync();
+            await commandHandler.InstallCommandsAsync();
+
+            await _client.SetStatusAsync(botStatus);
+            await _client.SetGameAsync(botPlayingStatus);
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
